71f53cf43c6f21eec652f2fa90ed77ea2b0bda8b
Ponte, Nadia <deponte.work@gmail.com>
2023-12-05 22:57:07 +0000

    Wrote questions for the quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,33 @@
+// index.html
+// Define the questions, choices, and answers
+const questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts",
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within _______ .",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses",
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _______ .",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above",
+    },
+    {
+        question: "String values must be enclosed within _______ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes",
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal /bash", "for loops", "console.log"],
+        answer: "console.log",
+    },
+];
+
+// Export the questions array
+export default questions; // doesn't work because we didn't learn yet how to use node
+
