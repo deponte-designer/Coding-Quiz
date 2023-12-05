7802f66d261a9786aafc3bb7fffaff8ff56cc566
Ponte, Nadia <deponte.work@gmail.com>
2023-12-05 23:05:50 +0000

    Added the pseudo code

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,33 +1,46 @@
 // index.html
-// Define the questions, choices, and answers
-const questions = [
-    {
-        question: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts",
-    },
-    {
-        question: "The condition in an if / else statement is enclosed within _______ .",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses",
-    },
-    {
-        question: "Arrays in JavaScript can be used to store _______ .",
-        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        answer: "all of the above",
-    },
-    {
-        question: "String values must be enclosed within _______ when being assigned to variables.",
-        choices: ["commas", "curly brackets", "quotes", "parentheses"],
-        answer: "quotes",
-    },
-    {
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        choices: ["JavaScript", "terminal /bash", "for loops", "console.log"],
-        answer: "console.log",
-    },
-];
 
-// Export the questions array
-export default questions; // doesn't work because we didn't learn yet how to use node
+// - Define the questions and the choices and the answers, put it in a variable in questions.js file
+// -------------- QUESTIONS -----------------------
+// Commonly used data types DO NOT include: 1. strings 2. booleans 3. alerts < 4. numbers
+// The condition in an if / esle statement is enclosed within ______ . 1. quotes 2. curly brackets 3. parentheses < 4. square brackets
+// Arrays in JavaScript can be used to store _______ . 1. numbers and strings 2. other arrays 3. booleans 4. all of the above <
+// String values must be enclosed within ______ when being assigned to variables. 1. commas 2. curly brackets 3. quotes < 4. parentheses
+// A very useful tool used during development and bebugging for printing content to the debugger is: 1. JavaScript 2. terminal /bash 3. for loops 4. console.log <
+//
+// -------------------------------------------------
+
+
+// // index.html
+// // Define the questions, choices, and answers
+// const questions = [
+//     {
+//         question: "Commonly used data types DO NOT include:",
+//         choices: ["strings", "booleans", "alerts", "numbers"],
+//         answer: "alerts",
+//     },
+//     {
+//         question: "The condition in an if / else statement is enclosed within _______ .",
+//         choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+//         answer: "parentheses",
+//     },
+//     {
+//         question: "Arrays in JavaScript can be used to store _______ .",
+//         choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+//         answer: "all of the above",
+//     },
+//     {
+//         question: "String values must be enclosed within _______ when being assigned to variables.",
+//         choices: ["commas", "curly brackets", "quotes", "parentheses"],
+//         answer: "quotes",
+//     },
+//     {
+//         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+//         choices: ["JavaScript", "terminal /bash", "for loops", "console.log"],
+//         answer: "console.log",
+//     },
+// ];
+
+// // Export the questions array
+// export default questions; // doesn't work because we didn't learn yet how to use node
 
