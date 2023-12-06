2b22dc4c251363c48959b151f001dbf4a3104e61
Ponte, Nadia <deponte.work@gmail.com>
2023-12-06 00:01:41 +0000

    Added pseudo code

---
+++ assets/js/scores.js
@@ -0,0 +1,7 @@
+// highscores.html
+// Pseudo code:
+// - Retrieve highscores from local storage
+// - sort the scores from higher score to lower score
+// - Display the highscores as a list
+// - When the user click on "Clear Highscores", clear local storage
+
